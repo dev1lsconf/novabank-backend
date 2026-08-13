02c80984be43ca8bd3b47c6245b2cd6cad68ff12
Eric Batista <ericbatista@gmail.com>
2026-08-14 01:25:30 +0200

    fix: agregar dependencias aws-lambda y crear archivo public.decorator.ts

--- src/common/decorators/idempotency-key.decorator.ts
+++ src/common/decorators/idempotency-key.decorator.ts
@@ -1,7 +1,4 @@
-import { SetMetadata, createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const IdempotencyKey = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string | undefined => {
